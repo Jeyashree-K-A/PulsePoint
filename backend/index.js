56ddf8f7316e0338fc7b595906d8d218182804d5
Jeyashree K A <23CSEB11Jeyashree@gmail.com>
2026-01-25 23:16:21 +0530

    Use environment variable for server port

--- backend/index.js
+++ backend/index.js
@@ -53,7 +53,7 @@ const server = http.createServer(app);
 /* ===============================
    ✅ START SERVER (ONLY HERE)
 ================================ */
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;;
 server.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
